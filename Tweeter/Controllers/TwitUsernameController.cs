6c6d841cce82362aae15c67afeda308079166a63
JZorvan <jzorvan@gmail.com>
2016-11-18 13:38:49 -0800

    push

---
+++ Tweeter/Controllers/TwitUsernameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tweeter.Controllers
+{
+    public class TwitUsernameController : Controller
+    {
+        // GET: TwitUsername
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
